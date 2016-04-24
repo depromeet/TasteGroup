b069e3cc93525b84a6641fbabbc28d42fd433204
jongwook <jongwook@mit.edu>
2016-04-25 04:09:32 +0900

    Added image feature

--- app/src/main/java/com/depromeet/tastegroup/RestInfoActivity.java
+++ app/src/main/java/com/depromeet/tastegroup/RestInfoActivity.java
@@ -1,12 +1,17 @@
 package com.depromeet.tastegroup;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,6 +27,7 @@ public class RestInfoActivity extends AppCompatActivity {
     public static final String REST_TYPE = "restType";
     private TextView nameText;
     private TextView phoneText;
+    private ImageView imgView;
     private int resId;
 
     public class Restaurant {
@@ -48,6 +54,7 @@ public class RestInfoActivity extends AppCompatActivity {
 
         nameText = (TextView) findViewById(R.id.Name);
         phoneText = (TextView) findViewById(R.id.Telephone);
+        imgView = (ImageView) findViewById(R.id.img);
         int resId = (Integer)getIntent().getExtras().get(REST_TYPE);
         nameText.setText(String.valueOf(resId));
 
@@ -61,6 +68,9 @@ public class RestInfoActivity extends AppCompatActivity {
                 //phoneText.setText("Telephone: " + String.valueOf(res.getTelephone()));
                 nameText.setText("Name: " + snapshot.child("Name").getValue(String.class));
                 phoneText.setText(("Telephone: " + snapshot.child("Telephone").getValue(Long.class)));
+                byte[] decodedBytes = Base64.decode(snapshot.child("Image").getValue(String.class), 0);
+                Bitmap bitImg = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+                imgView.setImageBitmap(bitImg);
             }
 
             @Override public void onCancelled(FirebaseError error) { }
