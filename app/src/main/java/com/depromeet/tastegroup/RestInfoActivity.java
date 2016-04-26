6e8c5338aaa330dbe920626550072ca02142d104
jongwook <jongwook@mit.edu>
2016-04-26 16:54:53 +0900

    added dialing function

--- app/src/main/java/com/depromeet/tastegroup/RestInfoActivity.java
+++ app/src/main/java/com/depromeet/tastegroup/RestInfoActivity.java
@@ -1,9 +1,11 @@
 package com.depromeet.tastegroup;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -11,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -28,6 +31,7 @@ public class RestInfoActivity extends AppCompatActivity {
     private TextView nameText;
     private TextView phoneText;
     private ImageView imgView;
+    private String phoneNumber;
     private int resId;
 
 
@@ -36,19 +40,21 @@ public class RestInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rest_info);
 
+        // Get the relevant views
         nameText = (TextView) findViewById(R.id.Name);
         phoneText = (TextView) findViewById(R.id.Telephone);
         imgView = (ImageView) findViewById(R.id.img);
         int resId = (Integer)getIntent().getExtras().get(REST_TYPE);
         nameText.setText(String.valueOf(resId));
 
+        // Connect with Firebase and get the relevant data
         Firebase baseRef = new Firebase(Constants.FIREBASE_URL);
         baseRef.child("Restaurants").child(String.valueOf(resId)).addListenerForSingleValueEvent(new ValueEventListener() {
-
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 nameText.setText(snapshot.child("Name").getValue(String.class));
-                phoneText.setText(("Telephone: " + snapshot.child("Telephone").getValue(Long.class)));
+                phoneNumber = snapshot.child("Telephone").getValue(String.class);
+                phoneText.setText("Telephone: " + phoneNumber);
                 byte[] decodedBytes = Base64.decode(snapshot.child("Image").getValue(String.class), 0);
                 Bitmap bitImg = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
                 imgView.setImageBitmap(bitImg);
@@ -57,5 +63,16 @@ public class RestInfoActivity extends AppCompatActivity {
             @Override public void onCancelled(FirebaseError error) { }
 
         });
+
+       // Button listen for dialing the restaurant
+        Button buttonDial = (Button) findViewById(R.id.button2);
+        buttonDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+phoneNumber));
+                startActivity(intent);
+           }
+       });
     }
 }
